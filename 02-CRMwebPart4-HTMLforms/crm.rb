9a0a3ea11d188ff7141ed7984a04a0bafe946372
Martin <martin.st8n@gmail.com>
2019-03-19 17:19:13 -0400

    created put route to take the updated contact info and save it to our database

--- 02-CRMwebPart4-HTMLforms/crm.rb
+++ 02-CRMwebPart4-HTMLforms/crm.rb
@@ -31,6 +31,23 @@ get '/contacts/:id' do
 
 end
 
+put '/contacts/:id' do
+  @contact = Contact.find_by({id: params[:id].to_i})
+
+  if @contact
+    @contact.update(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      note: params[:note],
+    )
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+
+end
+
 get '/contacts' do
 
   @contacts = Contact.all
